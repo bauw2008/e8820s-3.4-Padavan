c2d03cd8054ede25b97c32029a19e4d6819c47f3
bauw2008 <77882800+bauw2008@users.noreply.github.com>
2023-08-23 21:46:19 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -243,17 +243,25 @@ EEE
 fi
 }
 
-
+dl_adg(){
+logger -t "AdGuardHome" "下载AdGuardHome"
+#wget --no-check-certificate -O /tmp/AdGuardHome.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.101.0/AdGuardHome_linux_mipsle.tar.gz
+curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/chongshengB/rt-n56u/trunk/user/adguardhome/AdGuardHome
+if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
+logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
+nvram set adg_enable=0
+exit 0
+else
+logger -t "AdGuardHome" "AdGuardHome下载成功。"
+chmod 777 /tmp/AdGuardHome/AdGuardHome
+fi
+}
 
 start_adg(){
-        #chmod 755 /opt/adg/AdGuardHome
-	chmod 755 /media/AiDisk_a1/adg/AdGuardHome
-        mkdir -p /tmp/AdGuardHome
+    mkdir -p /tmp/AdGuardHome
 	mkdir -p /etc/storage/AdGuardHome
 	if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
-	#cp /opt/adg/AdGuardHome /tmp/AdGuardHome/AdGuardHome
-	cp /media/AiDisk_a1/adg/AdGuardHome /tmp/AdGuardHome/AdGuardHome
-
+	dl_adg
 	fi
 	getconfig
 	change_dns
